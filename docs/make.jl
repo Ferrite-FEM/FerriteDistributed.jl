f577e31402507a7a818bd0f3b940fb5417db6f83
termi-official <termi-official@users.noreply.github.com>
2023-06-07 21:03:03 +0200

    Documenter skeleton based on Ferrite.jl.

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,35 @@
+using TimerOutputs
+
+dto = TimerOutput()
+reset_timer!(dto)
+
+const liveserver = "liveserver" in ARGS
+
+if liveserver
+    using Revise
+    @timeit dto "Revise.revise()" Revise.revise()
+end
+
 using Documenter
 using FerriteDistributed
+
+# Generate examples
+include("generate.jl")
+
+GENERATEDEXAMPLES = [joinpath("examples", f) for f in (
+    "heat_equation.md",
+)]
 
 makedocs(
     sitename = "FerriteDistributed.jl",
     format = Documenter.HTML(),
     doctest = false,
     strict = false,
+    draft = liveserver,
     pages = Any[
         "Home" => "index.md",
-        "API Reference" => [
+        "Examples" => [GENERATEDEXAMPLES;],
+        "Reference" => [
             "reference/interface.md",
             "reference/grid.md",
             "reference/utils.md",
@@ -16,9 +37,16 @@ makedocs(
     ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+# if !liveserver
+#     @timeit dto "deploydocs" deploydocs(
+#         repo = "github.com/Ferrite-FEM/FerriteDistributed.jl.git",
+#         push_preview=true,
+#         versions = [
+#             "stable" => "v^",
+#             "v#.#",
+#             "dev" => "dev"
+#         ]
+#     )
+# end
+
+print_timer(dto)
