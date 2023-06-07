e786368175606e56a2c599ad9e2099f892c6043e
termi-official <termi-official@users.noreply.github.com>
2023-06-07 16:29:15 +0200

    Fix compile errors and add test.

--- ext/FerriteDistributedMetisPartitioning.jl
+++ ext/FerriteDistributedMetisPartitioning.jl
@@ -1,11 +1,12 @@
 
-module FerriteNODGridMetisPartitioning
+module FerriteDistributedMetisPartitioning
 
 # This extension requires modules as type parameters
 # https://github.com/JuliaLang/julia/pull/47749
 if VERSION >= v"1.10.0-DEV.90"
 
-using Metis
+using FerriteDistributed, Metis
+import FerriteDistributed: CoverTopology, PartitioningAlgorithm
 
 struct MetisPartitioning <: PartitioningAlgorithm.Ext{Metis}
     alg::Symbol
@@ -13,12 +14,10 @@ end
 
 PartitioningAlgorithm.Ext{Metis}(alg::Symbol) = MetisPartitioning(alg)
 
-"""
-"""
-function FerriteNODGrid.create_partitioning(grid::Grid{dim,C,T}, grid_topology::CoverTopology, n_partitions::Int, partition_alg::PartitioningAlgorithm.Ext{Metis}) where {dim,C,T}
+function FerriteDistributed.create_partitioning(grid::Grid{dim,C,T}, grid_topology::CoverTopology, n_partitions::Int, partition_alg::PartitioningAlgorithm.Ext{Metis}) where {dim,C,T}
     n_cells_global = getncells(grid)
     @assert n_cells_global > 0
-    
+
     if n_partitions == 1
         return ones(Metis.idx_t, n_cells_global)
     end
@@ -44,10 +43,10 @@ function FerriteNODGrid.create_partitioning(grid::Grid{dim,C,T}, grid_topology::
             adjncy
         ),
         n_partitions;
-        alg=partition_alg
+        alg=partition_alg.alg
     )
 end
 
 end # VERSION check
 
-end # module FerriteNODGridMetisPartitioning
+end # module FerriteDistributedMetisPartitioning
